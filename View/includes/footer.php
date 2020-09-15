ba0451565d83a3919495bdf56ec2ff7ebf0e4923
Edson Cabral Junior <e2cabraljunior@gmail.com>
2020-09-15 14:14:51 -0300

    Update footer.php

--- View/includes/footer.php
+++ View/includes/footer.php
@@ -3,8 +3,8 @@
             <div class="footer-copyright grey-text grey-darken-4">© 2017 Copyright by Edson & Alex, All rights reserved.</div>
         </div>
     </footer>
-    <script src="js/jquery-3.1.1.min.js"></script>
-    <script src="materialize/js/materialize.min.js"></script>
+    <script src="https://code.jquery.com/jquery-migrate-3.3.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB6q8Feib-wt9THnU--CoyMn6nZ8OBqkRw"></script>
     <script src="js/main.js"></script>
 </body>
